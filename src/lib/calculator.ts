06f93299fd52eb42a61d7edb4729a6b6e800a10c
Ricardo Racki <ricardo_racki@hotmail.com>
2023-12-24 19:39:33 -0300

    feat: Remove unused function

--- src/lib/calculator.ts
+++ src/lib/calculator.ts
@@ -3,11 +3,3 @@ const FACTOR = 15;
 export const convertKgInArroba = (v: number) => Number(v / FACTOR);
 
 export const convertArrobaInKg = (v: number) => Number(v * FACTOR);
-
-export const formatWithThousandDot = (v: string) =>
-  v
-    .split("")
-    .reverse()
-    .map((v, i) => (i % 3 == 0 ? `.${v}` : v))
-    .reverse()
-    .join();
